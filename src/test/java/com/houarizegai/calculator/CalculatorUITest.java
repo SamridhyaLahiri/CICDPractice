9730e8b0f710c5745b56121dc6fa077234050329
SamridhyaLahiri <36982281+SamridhyaLahiri@users.noreply.github.com>
2025-07-29 18:44:40 +0530

    Update CalculatorUITest.java

--- src/test/java/com/houarizegai/calculator/CalculatorUITest.java
+++ src/test/java/com/houarizegai/calculator/CalculatorUITest.java
@@ -18,7 +18,7 @@ class CalculatorUITest {
     @BeforeEach
     void setUp() {
         // Skip test if running in headless mode
-        Assume.assumeFalse(GraphicsEnvironment.isHeadless());
+        assumeFalse(GraphicsEnvironment.isHeadless());
         calculatorUI = new CalculatorUI();
     }
 
@@ -26,7 +26,7 @@ class CalculatorUITest {
     @CsvSource({"3,5,+,8", "2,8,-,-6", "44.5,10,*,445", "320,5,/,64", "3,5,%,3", "5,3,^,125"})
     void testCalculation(double firstNumber, double secondNumber, char operator, double expectedResult) {
         // Skip test if running in headless mode
-        Assume.assumeFalse(GraphicsEnvironment.isHeadless());
+        assumeFalse(GraphicsEnvironment.isHeadless());
         assertEquals(expectedResult, calculatorUI.calculate(firstNumber, secondNumber, operator));
     }
 }
